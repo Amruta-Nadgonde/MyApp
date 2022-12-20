8a203c1252c72c6fa37db604dbee514b6641791e
amruta-nadgonde <amruta.nadgonde@gmail.com>
2022-12-20 12:40:53 +0530

    initial commit by user A

---
+++ src/main/java/demo/Calculator.java
@@ -0,0 +1,22 @@
+package demo;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st no");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter second no");
+		int b = sc.nextInt();
+		
+		System.out.println("Addition :" + Maths.add(a, b));
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/main/java/demo/Maths.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Maths {
+
+	public static int add(int a, int b)
+	{
+		int c = a+b;
+		return c;
+	}
+}
