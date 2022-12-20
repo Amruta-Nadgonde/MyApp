b4d66c65ee5e93e335ea9a9c41b4c8757a3ae034
amruta-nadgonde <amruta.nadgonde@gmail.com>
2022-12-20 16:53:46 +0530

    new code in Dev1

---
+++ src/main/java/demo/Dev1.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Dev1 {
+
+	public static void main(String[] args) {
+
+			int x=10;
+			for(int i =0; i<9;i++)
+			{
+				x--;
+				System.out.println(x);
+			}
+
+	}
+
+}
