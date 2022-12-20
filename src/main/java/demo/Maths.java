402da36491804c3d52d9a2b61b9996bec4d17958
amruta-nadgonde <amruta.nadgonde@gmail.com>
2022-12-20 15:12:17 +0530

    added overloaded method

--- src/main/java/demo/Maths.java
+++ src/main/java/demo/Maths.java
@@ -8,6 +8,12 @@ public class Maths {
 		return c;
 	}
 	
+	public static int add(int a, int b, int d)
+	{
+		int c = a+b+d;
+		return c;
+	}
+	
 	public static int multiply(int a, int b)
 	{
 		int c = a*b;
